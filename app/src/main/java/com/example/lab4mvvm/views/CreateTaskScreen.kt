4b8f9058c6bc7b191e839d0a3acdfc546f2f97e1
G4lvez09 <00042424@uca.edu.sv>
2026-06-12 23:59:07 -0600

    Update CreateTaskScreen.kt

--- app/src/main/java/com/example/lab4mvvm/views/CreateTaskScreen.kt
+++ app/src/main/java/com/example/lab4mvvm/views/CreateTaskScreen.kt
@@ -1,16 +1,85 @@
-Button(
-onClick = {
+package com.example.lab4mvvm.views
 
-    val task = Task(
-        title = title,
-        description = description
-    )
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.lab4mvvm.model.Task
+import com.example.lab4mvvm.viewmodel.GeneralViewModel
 
-    viewModel.addTask(task)
-
-    navController.popBackStack()
-},
-enabled = title.isNotBlank()
+@Composable
+fun CreateTaskScreen(
+    navController: NavController,
+    viewModel: GeneralViewModel
 ) {
-    Text("Guardar")
+
+    var title by remember {
+        mutableStateOf("")
+    }
+
+    var description by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+
+        Text(
+            text = "Nueva Tarea",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = title,
+            onValueChange = {
+                title = it
+            },
+            label = {
+                Text("Título")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = description,
+            onValueChange = {
+                description = it
+            },
+            label = {
+                Text("Descripción")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(
+            onClick = {
+
+                val task = Task(
+                    title = title,
+                    description = description
+                )
+
+                viewModel.addTask(task)
+
+                navController.popBackStack()
+            },
+            enabled = title.isNotBlank()
+        ) {
+            Text("Guardar")
+        }
+    }
 }
